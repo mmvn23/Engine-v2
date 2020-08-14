1c3ec950941556273efe8aac127afb64d2e41d76
mmvn23 <mmartinsvn@gmail.com>
2020-08-14 16:36:31 -0500

    PN inheritance (yhold) funcionality added: if a part-number changes its code after budgeting, we can reference this part-number to one create during planning. E.g.: PN-013 was created after planning in order to substitute PN-001. Therefore, PN will inherit its budgeting data.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # TARGETS:
 # import data from excel: OK
 # cross data using a key OK
-# load YTD using objects
-# load YTD using arrays
-# calculate YTG based on yearly readjustments
+# load YTD using objects OK
+# load YTD using arrays LATER
+# Implement inheritance (predecessor) OK
+# calculate ytg based on yearly readjustments
 # load and save data using CSV
 # save date on a excel file
 # calculate savings
@@ -13,19 +14,19 @@ import pandas as pd
 import numpy as np
 
 
-class Part_Number_Class:
+class PartNumberClass:
 
-    def __init__(self,part_number_code,category= "", price_budget = 0,volume_budget = [0] * 12):
+    def __init__(self, code, category="", price_budget=0, volume_budget=[0] * 12):
         months_per_year = 12
         start_var = [0] * months_per_year
-        self.part_number_code = part_number_code
+        self.code = code
         self.category = category
         self.price_budget = price_budget
-        self.price_YTD = start_var
-        self.price_YTG = start_var
+        self.price_ytd = start_var
+        self.price_ytg = start_var
         self.volume_budget = volume_budget
-        self.volume_YTD = start_var
-        self.volume_YTG = start_var
+        self.volume_ytd = start_var
+        self.volume_ytg = start_var
         self.part_number_ref = ""
 
     def __str__(self):
@@ -34,53 +35,59 @@ class Part_Number_Class:
                              "Price:\n"\
                              "  Budget: {2}\n"\
                              "  YTD:{3}\n"\
-                             "  YTG: {4}\n"\
+                             "  ytg: {4}\n"\
                              "Volume:\n" \
                              "  Budget: {5}\n" \
                              "  YTD:{6}\n" \
-                             "  YTG: {7}\n" \
-                             .format(self.part_number_code,self.category,self.price_budget,self.price_YTD,self.price_YTG,self.volume_budget,self.volume_YTD,self.volume_YTG)
+                             "  ytg: {7}\n" \
+                             .format(self.code, self.category, self.price_budget, self.price_ytd,
+                                     self.price_ytg, self.volume_budget, self.volume_ytd, self.volume_ytg)
         return part_number_string
 
-    def update_YTD(self,price_YTD,volume_YTD):
-        self.price_YTD = price_YTD
-        self.volume_YTD = volume_YTD
+    def update_ytd(self, price_ytd, volume_ytd):
+        self.price_ytd = price_ytd
+        self.volume_ytd = volume_ytd
 
-def update_YTD_from_list_search(part_number_code,part_number_list,price_YTD,volume_YTD):
+
+def update_ytd_from_list_search(code, pn_list, price_ytd, volume_ytd):
     part_of_list = False
-    for item in part_number_list:
-        if item.part_number_code == part_number_code:
-            item.update_YTD(price_YTD, volume_YTD)
+    for item in pn_list:
+        if item.code == code:
+            item.update_ytd(price_ytd, volume_ytd)
             part_of_list = True
     return part_of_list
 
-def part_number_list_search(part_number_code,part_number_list):
+
+def part_number_list_search(code, pn_list):
     right_part_number = False
-    for item in part_number_list:
-        if item.part_number_code == part_number_code:
+    for item in pn_list:
+        if item.code == code:
             right_part_number = item
     return right_part_number
 
-def yhold_search(part_number_code,yhold_list):
-    yhold = False
-    for i in range(len(yhold_list)):
-        if yhold_list[i,1] == part_number_code: yhold = yhold_list[i,0]
-    return yhold
+
+def predecessor_search(code, predecessor_list):
+    predecessor = False
+    for i in range(len(predecessor_list)):
+        if predecessor_list[i, 1] == code:
+            predecessor = predecessor_list[i, 0]
+    return predecessor
+
 
 savingsFile = pd.ExcelFile(r'.\inputs\Test.xlsx')
-budget_raw_array = np.array(pd.read_excel(savingsFile,sheet_name="PAF"))
+budget_raw_array = np.array(pd.read_excel(savingsFile, sheet_name="PAF"))
 
-part_number_code_list = budget_raw_array[:,0]
-category_list = budget_raw_array[:,1]
+code_list = budget_raw_array[:, 0]
+category_list = budget_raw_array[:, 1]
 # budget information
-price_budget_list = budget_raw_array[:,2]
-volume_budget_list = budget_raw_array[:,[3,14]]
+price_budget_list = budget_raw_array[:, 2]
+volume_budget_list = budget_raw_array[:, [3, 14]]
 # YTD information
 
 # creating objects and assigning budgets
-part_number_amount = len(part_number_code_list)
-part_number_list = [Part_Number_Class(part_number_code_list[i],category_list[i],price_budget_list[i])
-                    for i in range (part_number_amount)]
+part_number_amount = len(code_list)
+part_number_list = [PartNumberClass(code_list[i], category_list[i], price_budget_list[i])
+                    for i in range(part_number_amount)]
 
 # for item in part_number_list:
 #     print(item)
@@ -88,23 +95,26 @@ part_number_list = [Part_Number_Class(part_number_code_list[i],category_list[i],
 # assigning YTD
 report_month = 4
 
-ytd_raw_array = np.array(pd.read_excel(savingsFile,sheet_name="YTD"))
+ytd_raw_array = np.array(pd.read_excel(savingsFile, sheet_name="YTD"))
 # print(ytd_raw_array.shape)
-part_number_code_list_YTD = ytd_raw_array[:,0]
-price_YTD_list = ytd_raw_array[:,range(1,1+report_month)]
-volume_YTD_list = ytd_raw_array[:,range(13,13+report_month)]
-yhold_match = np.array(pd.read_excel(savingsFile,sheet_name="Yhold"))
+code_list_YTD = ytd_raw_array[:, 0]
+price_ytd_list = ytd_raw_array[:, range(1, 1+report_month)]
+volume_ytd_list = ytd_raw_array[:, range(13, 13+report_month)]
+predecessor_match = np.array(pd.read_excel(savingsFile, sheet_name="Yhold"))
 
-for i in range(len(part_number_code_list_YTD)):
-    x = update_YTD_from_list_search(part_number_code_list_YTD[i],part_number_list,price_YTD_list[i,:],
-                                    volume_YTD_list[i,:])
-    if not(x):
-        part_number_code_from_yhold = yhold_search(part_number_code_list_YTD[i],yhold_match)
-        part_number_from_yhold = part_number_list_search(part_number_code_from_yhold,part_number_list)
-        category_from_yhold = part_number_from_yhold.category
-        price_budget_from_yhold = part_number_from_yhold.price_budget
-        part_number_add = Part_Number_Class(part_number_code_list_YTD[i],category_from_yhold,price_budget_from_yhold)
+for i in range(len(code_list_YTD)):
+    x = update_ytd_from_list_search(code_list_YTD[i], part_number_list, price_ytd_list[i, :],
+                                    volume_ytd_list[i, :])
+    if not x:
+        code_from_predecessor = predecessor_search(code_list_YTD[i], predecessor_match)
+        part_number_from_predecessor = part_number_list_search(code_from_predecessor, part_number_list)
+        category_from_predecessor = part_number_from_predecessor.category
+        price_budget_from_predecessor = part_number_from_predecessor.price_budget
+        part_number_add = PartNumberClass(code_list_YTD[i], category_from_predecessor, price_budget_from_predecessor)
         part_number_list.append(part_number_add)
+        y = update_ytd_from_list_search(part_number_add.code, part_number_list, price_ytd_list[i, :],
+                                        volume_ytd_list[i, :])
+
 print(len(part_number_list))
 for i in range(len(part_number_list)):
     print(i)
@@ -112,7 +122,7 @@ for i in range(len(part_number_list)):
 
 # print(part_number_list_YTD)
 
-#ytg_raw_array = np.array(pd.read_excel(savingsFile,sheet_name="YTG"))
+#ytg_raw_array = np.array(pd.read_excel(savingsFile,sheet_name="ytg"))
 
 
 
@@ -126,16 +136,16 @@ for i in range(len(part_number_list)):
 
 
 #
-# # assigning YTD and YTG
+# # assigning YTD and ytg
 # month_report = 3
-# price_YTD #next step is assigning columns from master data array
-# volume_YTD
-# price_YTG
-# volume_YTG
+# price_ytd #next step is assigning columns from master data array
+# volume_ytd
+# price_ytg
+# volume_ytg
 # master_data_array
 #
 # for item in part_number_list:
-#     item.update_YTD_and_YTG()
+#     item.update_ytd_and_ytg()
 #     print(item)
 
 
