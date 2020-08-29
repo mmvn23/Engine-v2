0f03c59aae232c390aba77765c0fb806c153520f
mmvn23 <mmartinsvn@gmail.com>
2020-08-29 11:24:45 -0500

    Refactored column integrity.

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 #############################                1) DATA CLEANING                     ######################################
 
-def file_column_integrity(dataframe, column_expected_list):
-    error_list = []
+def file_column_integrity(dataframe, column_expected_list, error_str, old_error_list=[]):
+    new_error_list = old_error_list
     columns_to_delete = []
     raw_column_list = dataframe.columns.tolist()
     column_integrity = True
@@ -9,7 +9,7 @@ def file_column_integrity(dataframe, column_expected_list):
     for ii in range(1, len(column_expected_list)):
         is_part_of_raw_column_list = column_expected_list[ii] in raw_column_list
         if not is_part_of_raw_column_list:
-            error_list.append(column_expected_list[ii])
+            new_error_list.append(error_str.format(column_expected_list[ii]))
         column_integrity = column_integrity and is_part_of_raw_column_list
 
     for jj in range(1, len(raw_column_list)):
@@ -17,7 +17,11 @@ def file_column_integrity(dataframe, column_expected_list):
         if not is_part_of_expected_list:
             columns_to_delete.append(raw_column_list[jj])
 
-    return [columns_to_delete, column_integrity, error_list]
+    if not is_part_of_raw_column_list:
+        print(new_error_list)
+
+    return [columns_to_delete, new_error_list]
+
 
 def list_differential(old_list, new_list):
     item_diff = []
@@ -36,3 +40,26 @@ def generate_wide_act_raw_column_expected_list(old_list, report_month, base_stri
         new_list.append(base_string + str(ii))
 
     return new_list
+
+
+def generate_wide_act_column_conversion(base_dict, report_month, base_string):
+    final_dict = base_dict
+    for jj in range(1, report_month+1):
+         final_dict[base_string + str(jj)] = str(jj)
+
+    return final_dict
+
+
+def merge_and_drop(dataframe, df_equalizer, df_merge_column, equalizer_merge_column, new_column_name):
+    dataframe = dataframe.merge(df_equalizer, left_on=df_merge_column, right_on=equalizer_merge_column, how='left')
+    if df_merge_column in dataframe.columns:
+        dataframe.drop(df_merge_column, axis=1, inplace=True)
+    if equalizer_merge_column in dataframe.columns:
+        dataframe.drop(equalizer_merge_column, axis=1, inplace=True)
+    equalizer_column_names = df_equalizer.columns
+    dataframe.rename(columns={equalizer_column_names[1]: new_column_name}, inplace=True)
+
+    return dataframe
+
+
+
--- main.py
+++ main.py
@@ -39,19 +39,20 @@ bgt_volume_column = 'bgt_volume'
 df_wide_bgt_raw_column_expected_list = ['Part Number', 'Description', 'Category', 'Plant', 'PAF price', 'FX', 'Unity',
                                         '1 or 1,000? ', 'PL or BS?', 'vol 01', 'vol 02', 'vol 03', 'vol 04', 'vol 05',
                                         'vol 06', 'vol 07', 'vol 08', 'vol 09', 'vol 10', 'vol 11', 'vol 12']
-df_long_bgt_column_list = [code_column, description_column, category_column, location_column, bgt_price_column,
-                           bgt_currency_column, bgt_uom_column, bgt_per_column, savings_type_column,bgt_volume_column,
-                           bgt_month_column]
 
-bgt_columns_conversion = {df_wide_bgt_raw_column_expected_list[0]: df_long_bgt_column_list[0],
-                          df_wide_bgt_raw_column_expected_list[1]: df_long_bgt_column_list[1],
-                          df_wide_bgt_raw_column_expected_list[2]: df_long_bgt_column_list[2],
-                          df_wide_bgt_raw_column_expected_list[3]: df_long_bgt_column_list[3],
-                          df_wide_bgt_raw_column_expected_list[4]: df_long_bgt_column_list[4],
-                          df_wide_bgt_raw_column_expected_list[5]: df_long_bgt_column_list[5],
-                          df_wide_bgt_raw_column_expected_list[6]: df_long_bgt_column_list[6],
-                          df_wide_bgt_raw_column_expected_list[7]: df_long_bgt_column_list[7],
-                          df_wide_bgt_raw_column_expected_list[8]: df_long_bgt_column_list[8],
+df_long_bgt_column_list_equalization = [code_column, description_column, category_column, location_column,
+                                           bgt_price_column, bgt_currency_column, bgt_uom_column, bgt_per_column,
+                                           savings_type_column, bgt_volume_column, bgt_month_column]
+
+bgt_column_conversion = {df_wide_bgt_raw_column_expected_list[0]: code_column,
+                          df_wide_bgt_raw_column_expected_list[1]: description_column,
+                          df_wide_bgt_raw_column_expected_list[2]: category_report_column,
+                          df_wide_bgt_raw_column_expected_list[3]: location_report_column,
+                          df_wide_bgt_raw_column_expected_list[4]: bgt_price_column,
+                          df_wide_bgt_raw_column_expected_list[5]: bgt_currency_report_column,
+                          df_wide_bgt_raw_column_expected_list[6]: bgt_uom_report_column,
+                          df_wide_bgt_raw_column_expected_list[7]: bgt_per_column,
+                          df_wide_bgt_raw_column_expected_list[8]: savings_type_report_column,
                           df_wide_bgt_raw_column_expected_list[9]: '1',
                           df_wide_bgt_raw_column_expected_list[10]: '2',
                           df_wide_bgt_raw_column_expected_list[11]: '3',
@@ -96,8 +97,9 @@ act_volume_per_column = 'act_volume_per'
 act_month_column = 'month'
 act_volume_column = 'act_volume'
 
-df_wide_act_raw_column_expected_list = ['Part Number', 'Description', 'Category', 'Plant', 'FX',
-                                        'PL or BS?']
+df_wide_act_raw_column_expected_list = ['Part Number', 'Description', 'Category', 'Plant', 'FX', 'Unity',
+                                        'PL or BS?'] # input has only 1 UoM, therefore we will have to adjust
+
 volume_act_base_string = "v"
 df_wide_act_raw_column_expected_list = fc.generate_wide_act_raw_column_expected_list(df_wide_act_raw_column_expected_list,
                                                                                   report_month, volume_act_base_string)
@@ -105,9 +107,24 @@ price_act_base_string = "P"
 df_wide_act_raw_column_expected_list = fc.generate_wide_act_raw_column_expected_list(df_wide_act_raw_column_expected_list,
                                                                                   report_month, price_act_base_string)
 df_long_act_column_list = [code_column, description_column, category_column, location_column, act_price_column,
-                           act_currency_column, act_price_uom_column, act_price_per_column, act_volume_column, 
+                           act_currency_column, act_price_uom_column, act_price_per_column, act_volume_column,
                            act_volume_uom_column, act_volume_per_column, act_month_column]
-# act_columns_conversion
+act_volume_columns_conversion_base = {df_wide_act_raw_column_expected_list[0]: code_column,
+                                      df_wide_act_raw_column_expected_list[1]: description_column,
+                                      df_wide_act_raw_column_expected_list[2]: category_report_column,
+                                      df_wide_act_raw_column_expected_list[3]: location_report_column,
+                                      df_wide_act_raw_column_expected_list[4]: act_currency_report_column,
+                                      df_wide_act_raw_column_expected_list[5]: act_volume_uom_report_column,
+                                      df_wide_act_raw_column_expected_list[6]: savings_type_report_column}
+
+act_volume_column_conversion = fc.generate_wide_act_column_conversion(act_volume_columns_conversion_base,
+                                                                      report_month, volume_act_base_string)
+
+act_price_columns_conversion_base = {df_wide_act_raw_column_expected_list[0]: code_column,
+                                     df_wide_bgt_raw_column_expected_list[6]: act_price_uom_report_column}
+
+act_price_column_conversion = fc.generate_wide_act_column_conversion(act_price_columns_conversion_base, report_month,
+                                                                     price_act_base_string)
 
 #############################                1) DATA CLEANING                     ######################################
 ########################    Load excel files    ########################
@@ -125,24 +142,17 @@ df_savings_type_equalizer = pd.read_excel(filename_savings_type_equalization)
 df_wide_act = pd.read_excel(filename_act)
 # budget, actuals, forecast, baseline, UoM and FX and project mapping
 
-######################## Check if all expected columns are on file ########################
+######################## Check column integrity ########################
 ### budget ###
-[columns_to_delete_bgt, is_bgt_raw_column_integrous, bgt_error_list] = fc.file_column_integrity(df_wide_bgt,
-                                                                                   df_wide_bgt_raw_column_expected_list)
-error_list = '''MISSING COLUMNS: \non Bgt file: {}\n'''.format(bgt_error_list)
-if not is_bgt_raw_column_integrous:
-    print(error_list)
-    exit()
+bgt_error_str = '''MISSING COLUMNS: \non Bgt file: {}\n'''
+[columns_to_delete_bgt, error_list] = fc.file_column_integrity(df_wide_bgt, df_wide_bgt_raw_column_expected_list,
+                                                               bgt_error_str)
 
 ### actuals ###
-[columns_to_delete_act, is_act_raw_column_integrous, act_error_list] = fc.file_column_integrity(df_wide_act,
-                                                                                   df_wide_act_raw_column_expected_list)
-act_error_list = '''on Act file: {}\n'''.format(act_error_list)
-error_list = error_list + act_error_list
-if not is_act_raw_column_integrous:
-    print(error_list)
-    exit()
-
+act_error_str = act_error_list = '''on Act file: {}\n'''
+[columns_to_delete_act, error_list] = fc.file_column_integrity(df_wide_act, df_wide_act_raw_column_expected_list,
+                                                               act_error_str, error_list)
+print(error_list)
 ######################## Drop extra columns ########################
 ### budget ###
 df_wide_bgt.drop(columns=columns_to_delete_bgt, inplace=True)
@@ -151,7 +161,8 @@ df_wide_act.drop(columns=columns_to_delete_act, inplace=True)
 
 ######################## Change column names ########################
 ### budget ###
-df_wide_bgt.rename(columns=bgt_columns_conversion, inplace=True)
+df_wide_bgt.rename(columns=bgt_column_conversion, inplace=True)
+
 ### actuals ###
 # Breaking dataframe in two before changing names before changing format
 df_act_column_list = df_wide_act_raw_column_expected_list
@@ -160,79 +171,82 @@ df_wide_volume_act = df_wide_act.filter(items=df_act_column_list[0:desired_colum
 
 desired_column_pr_act_list = df_wide_act_raw_column_expected_list[desired_column_vol_act:len(df_act_column_list)]
 desired_column_pr_act_list.append(df_act_column_list[0])
+desired_column_pr_act_list.append(df_act_column_list[5])
 df_wide_price_act = df_wide_act.filter(items=desired_column_pr_act_list, axis=1)
-print(df_wide_price_act.columns)
 
-# df_wide_act.rename(columns=act_columns_conversion, inplace=True)
+df_wide_volume_act.rename(columns=act_volume_column_conversion, inplace=True)
+df_wide_price_act.rename(columns=act_price_column_conversion, inplace=True)
 
-### terminology equalization
-# category
+######################## Terminology equalization ########################
 df_category_equalizer.rename(columns=category_equalizer_columns_conversion, inplace=True)
-
-df_wide_bgt = pd.merge(df_wide_bgt, df_category_equalizer, on=category_report_column, how='left')
-df_wide_bgt.drop(category_report_column, axis=1, inplace=True)
-
-# location
 df_location_equalizer.rename(columns=location_equalizer_columns_conversion, inplace=True)
-
-df_wide_bgt = pd.merge(df_wide_bgt, df_location_equalizer, on=location_report_column, how='left')
-df_wide_bgt.drop(location_report_column, axis=1, inplace=True)
-
-# currency
 df_currency_equalizer.rename(columns=currency_equalizer_columns_conversion, inplace=True)
-
-df_wide_bgt = pd.merge(df_wide_bgt, df_currency_equalizer, on=bgt_currency_report_column, how='left')
-df_wide_bgt.drop(bgt_currency_report_column, axis=1, inplace=True)
-
-# UoM
 df_uom_equalizer.rename(columns=uom_equalizer_columns_conversion, inplace=True)
-
-df_wide_bgt = pd.merge(df_wide_bgt, df_uom_equalizer, on=bgt_uom_report_column, how='left')
-df_wide_bgt.drop(bgt_uom_report_column, axis=1, inplace=True)
-
-# savings type
 df_savings_type_equalizer.rename(columns=savings_type_equalizer_columns_conversion, inplace=True)
 
-df_wide_bgt = pd.merge(df_wide_bgt, df_savings_type_equalizer, on=savings_type_report_column, how='left')
-df_wide_bgt.drop(savings_type_report_column, axis=1, inplace=True)
+### budget ###
+df_wide_bgt = fc.merge_and_drop(df_wide_bgt, df_category_equalizer, category_report_column, category_report_column,
+                                category_column)
+df_wide_bgt = fc.merge_and_drop(df_wide_bgt, df_location_equalizer, location_report_column, location_report_column,
+                                location_column)
+df_wide_bgt = fc.merge_and_drop(df_wide_bgt, df_currency_equalizer, bgt_currency_report_column,
+                                bgt_currency_report_column, bgt_currency_column)
+df_wide_bgt = fc.merge_and_drop(df_wide_bgt, df_uom_equalizer, bgt_uom_report_column,
+                                bgt_uom_report_column, bgt_uom_column)
+df_wide_bgt = fc.merge_and_drop(df_wide_bgt, df_savings_type_equalizer, savings_type_report_column,
+                                savings_type_report_column, savings_type_column)
 
-### verify NaN and prepare a PN with errors
+### actuals ###
+df_wide_volume_act = fc.merge_and_drop(df_wide_volume_act, df_category_equalizer, category_report_column,
+                                       category_report_column, category_column)
+df_wide_volume_act = fc.merge_and_drop(df_wide_volume_act, df_location_equalizer, location_report_column,
+                                       location_report_column, location_column)
+df_wide_volume_act = fc.merge_and_drop(df_wide_volume_act, df_currency_equalizer, act_currency_report_column,
+                                       bgt_currency_report_column, act_currency_column)
+df_wide_volume_act = fc.merge_and_drop(df_wide_volume_act, df_uom_equalizer, act_volume_uom_report_column,
+                                       bgt_uom_report_column, act_volume_uom_column)
+df_wide_price_act = fc.merge_and_drop(df_wide_price_act, df_uom_equalizer, act_price_uom_report_column,
+                                      bgt_uom_report_column, act_price_uom_column)
+df_wide_volume_act = fc.merge_and_drop(df_wide_volume_act, df_savings_type_equalizer, savings_type_report_column,
+                                       savings_type_report_column, savings_type_column)
 
-old_part_number_bgt_list = df_wide_bgt[code_column]
-old_part_number_bgt_list = [str(item) for item in old_part_number_bgt_list]
-
-df_wide_bgt.dropna(inplace=True)
-
-new_part_number_bgt_list = df_wide_bgt[code_column]
-new_part_number_bgt_list = [str(item) for item in new_part_number_bgt_list]
-
-missing_codes_bgt = fc.list_differential(old_part_number_bgt_list, new_part_number_bgt_list)
-error_list_bgt = '''PART-NUMBERS W/ PROBLEMS: \non Bgt file: {}'''.format(missing_codes_bgt)
-
-error_list = error_list + error_list_bgt
-
-
-### from wide to long format
-bgt_id_vars = [code_column, description_column, category_column, location_column, bgt_price_column, bgt_currency_column,
-               bgt_uom_column, bgt_per_column, savings_type_column]
-df_long_bgt = pd.melt(frame=df_wide_bgt, id_vars=bgt_id_vars, value_vars=None, var_name=bgt_month_column,
-                      value_name=bgt_volume_column)
-
-### convert data types
-df_long_bgt.astype({bgt_price_column: float, bgt_volume_column: float, bgt_per_column: float, bgt_month_column: int})
-
-### convert price and volume into budget price UoM
-# not applied for budget file
-
-
-
-
-### Reorder columns
-df_long_bgt = df_long_bgt[df_long_bgt_column_list]
-
-### save data-frame on CSV
-df_long_bgt.to_csv(filename_bgt_csv)
-
+# ### verify NaN and prepare a PN with errors
+#
+# old_part_number_bgt_list = df_wide_bgt[code_column]
+# old_part_number_bgt_list = [str(item) for item in old_part_number_bgt_list]
+#
+# df_wide_bgt.dropna(inplace=True)
+#
+# new_part_number_bgt_list = df_wide_bgt[code_column]
+# new_part_number_bgt_list = [str(item) for item in new_part_number_bgt_list]
+#
+# missing_codes_bgt = fc.list_differential(old_part_number_bgt_list, new_part_number_bgt_list)
+# error_list_bgt = '''PART-NUMBERS W/ PROBLEMS: \non Bgt file: {}'''.format(missing_codes_bgt)
+#
+# error_list = error_list + error_list_bgt
+#
+#
+# ### from wide to long format
+# bgt_id_vars = [code_column, description_column, category_column, location_column, bgt_price_column, bgt_currency_column,
+#                bgt_uom_column, bgt_per_column, savings_type_column]
+# df_long_bgt = pd.melt(frame=df_wide_bgt, id_vars=bgt_id_vars, value_vars=None, var_name=bgt_month_column,
+#                       value_name=bgt_volume_column)
+#
+# ### convert data types
+# df_long_bgt.astype({bgt_price_column: float, bgt_volume_column: float, bgt_per_column: float, bgt_month_column: int})
+#
+# ### convert price and volume into budget price UoM
+# # not applied for budget file
+#
+#
+#
+#
+# ### Reorder columns
+# df_long_bgt = df_long_bgt[df_long_bgt_column_list_equalization]
+#
+# ### save data-frame on CSV
+# df_long_bgt.to_csv(filename_bgt_csv)
+#
 
 
 
